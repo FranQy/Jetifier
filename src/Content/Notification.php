91389f04ba90c26bd5f7b9e21c034b4124d533f6
PK <dev.franqy@gmail.com>
2017-12-10 16:05:33 +0100

    create Notification payload

---
+++ src/Content/Notification.php
@@ -0,0 +1,197 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pk
+ * Date: 10.12.2017
+ * Time: 15:19
+ */
+
+namespace Jetifier\Content;
+
+/**
+ * @link https://firebase.google.com/docs/cloud-messaging/http-server-ref#notification-payload-support
+ */
+class Notification implements \JsonSerializable
+{
+    private $data = [
+        'title' => null,
+        'body' => null,
+        'sound' => null,
+        'badge' => null,
+        'click_action' => null,
+        'icon' => null,
+        'color' => null,
+        'tag' => null
+    ];
+
+
+    /**
+     * Notification constructor.
+     * @param string $title The notification's title.  This field is not visible on iOS phones and tablets.
+     */
+    public function __construct(?string $title)
+    {
+        $this->data['title'] = $title;
+    }
+
+    /**
+     * @param $name
+     * @param $value
+     * @throws \RuntimeException
+     */
+    public function __set($name, $value)
+    {
+        throw new \RuntimeException('Cannot add custom param to notification');
+    }
+
+    /**
+     * The notification's body text.
+     * devices: All
+     * @param string $body
+     * @return Notification
+     */
+    public function setBody(string $body)
+    {
+        $this->data['body'] = $body;
+        return $this;
+    }
+
+    /**
+     * The sound to play when the device receives the notification.
+     * devices: IOS, Android
+     * @param string $sound
+     * @return Notification
+     */
+    public function setSound(string $sound)
+    {
+        $this->data['sound'] = $sound;
+        return $this;
+    }
+
+    /**
+     * The value of the badge on the home screen app icon.
+     *
+     * If not specified, the badge is not changed.
+     *
+     * If set to 0, the badge is removed.
+     *
+     * devices: IOS
+     *
+     * @param int $badge
+     * @return Notification
+     * @throws \InvalidArgumentException
+     */
+    public function setBadge(int $badge)
+    {
+        $this->checkBadge($badge);
+        $this->data['badge'] = $badge;
+        return $this;
+    }
+
+    /**
+     * @param int $badge
+     * @throws \InvalidArgumentException
+     */
+    private function checkBadge(int $badge)
+    {
+        if ($badge < 0) {
+            throw new \InvalidArgumentException('Badge must by minimum 0');
+        }
+    }
+
+    /**
+     * The action associated with a user click on the notification.
+     *
+     * devices: All
+     * @param string $clickAction
+     * @return Notification
+     */
+    public function setClickAction(string $clickAction)
+    {
+        $this->data['click_action'] = $clickAction;
+        return $this;
+    }
+
+    /**
+     * The notification's icon.
+     *
+     * Sets the notification icon to myicon for drawable resource myicon.
+     * If you don't send this key in the request,
+     * FCM displays the launcher icon specified in your app manifest.
+     *
+     * devices: Android, Web
+     * @param mixed $icon
+     * @return Notification
+     */
+    public function setIcon(string $icon)
+    {
+        $this->data['icon'] = $icon;
+        return $this;
+    }
+
+    /**
+     * The notification's icon color, expressed in #rrggbb format.
+     *
+     * devices: Android
+     * @param mixed $color
+     * @return Notification
+     * @throws \InvalidArgumentException
+     */
+    public function setColor(string $color)
+    {
+        $this->checkColor($color);
+        $this->data['color'] = $color;
+        return $this;
+    }
+
+    /**
+     * @param string $color
+     * @throws \InvalidArgumentException
+     */
+    private function checkColor(string $color)
+    {
+        $regex = '/^#[\dA-Fa-f]{6}$/';
+
+        if (empty(preg_match($regex, $color))) {
+            throw new \InvalidArgumentException('Color must be in #rrggbb format');
+        }
+    }
+
+    /**
+     * Identifier used to replace existing notifications in the notification drawer.
+     *
+     * If not specified, each request creates a new notification.
+     *
+     * If specified and a notification with the same tag is already being shown,
+     * the new notification replaces the existing one in the notification drawer.
+     *
+     * devices: Android
+     * @param mixed $tag
+     * @return Notification
+     */
+    public function setTag(string $tag)
+    {
+        $this->data['tag'] = $tag;
+        return $this;
+    }
+
+
+    /**
+     * Specify data which should be serialized to JSON
+     * @link http://php.net/manual/en/jsonserializable.jsonserialize.php
+     * @return mixed data which can be serialized by <b>json_encode</b>,
+     * which is a value of any type other than a resource.
+     * @since 5.4.0
+     */
+    public function jsonSerialize()
+    {
+        return $this->getFilteredData();
+    }
+
+    private function getFilteredData()
+    {
+        return array_filter($this->data, function ($value) {
+            return $value !== null;
+        });
+    }
+}
---
+++ tests/Content/NotificationTest.php
@@ -0,0 +1,85 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pk
+ * Date: 10.12.2017
+ * Time: 15:19
+ */
+
+namespace Jetifier\Tests\Content;
+
+use Jetifier\Content\Notification;
+use PHPUnit\Framework\TestCase;
+
+class NotificationTest extends TestCase
+{
+
+    public function testNotificationSerialization()
+    {
+        $notification = new Notification('title');
+        $notification->setBody('body');
+        $expect = ['title' => 'title', 'body' => 'body'];
+
+        $this->assertEquals($expect, $notification->jsonSerialize());
+    }
+
+    public function testEmptyNotificationSerialization()
+    {
+        $notification = new Notification(null);
+        $this->assertEquals([], $notification->jsonSerialize());
+    }
+
+    public function testSetColor()
+    {
+        $notification = new Notification(null);
+        $notification->setColor('#09afAF');
+        $this->assertEquals(['color' => '#09afAF'], $notification->jsonSerialize());
+    }
+
+    public function testSetColorEmptyException()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $notification = new Notification(null);
+        $notification->setColor('');
+    }
+
+    public function testSetColorTooShortException()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $notification = new Notification(null);
+        $notification->setColor('#123');
+
+    }
+
+    public function testSetColorBadValueException()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $notification = new Notification(null);
+        $notification->setColor('#1234g7');
+
+    }
+
+    public function testSetBadge()
+    {
+        $notification = new Notification(null);
+        $notification->setBadge(5);
+        $this->assertEquals(['badge' => 5], $notification->jsonSerialize());
+    }
+
+    public function testSetBadgeZero()
+    {
+        $notification = new Notification(null);
+        $notification->setBadge(0);
+        $this->assertEquals(['badge' => 0], $notification->jsonSerialize());
+    }
+
+
+    public function testSetBadgeException()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $notification = new Notification(null);
+        $notification->setBadge(-1);
+
+    }
+}
+
