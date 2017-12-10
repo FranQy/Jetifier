42c52b0411b31c979fedec1fbb0e48e6316a5e4c
PK <dev.franqy@gmail.com>
2017-12-10 12:46:57 +0100

    create Topic recipient

---
+++ src/Recipient/Topic.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pk
+ * Date: 10.12.2017
+ * Time: 12:03
+ */
+
+namespace Jetifier\Recipient;
+
+
+use Jetifier\Exceptions\BadRecipientIdentifierException;
+
+/**
+ * Class Topic
+ * @package Jetifier\Recipient
+ */
+class Topic implements RecipientInterface
+{
+
+    private static $formatRegex = '/[a-zA-Z0-9-_.~%]+/';
+    private static $prefix = '/topics/';
+    private $topic;
+
+    /**
+     * Topic constructor.
+     * @param string $topic Topic in format maches "[a-zA-Z0-9-_.~%]+" regex
+     */
+    public function __construct(string $topic)
+    {
+        $this->topic = $topic;
+    }
+
+    /**
+     * @return string
+     * @throws BadRecipientIdentifierException
+     */
+    public function getIdentifier(): string
+    {
+        if (empty($this->topic)) {
+            throw new BadRecipientIdentifierException('Empty topic');
+        }
+        if (!$this->testFormat()) {
+            throw new BadRecipientIdentifierException('Bad topic format');
+        }
+
+        return self::$prefix . $this->topic;
+    }
+
+    private function testFormat(): bool
+    {
+        $out = [];
+        preg_match(self::$formatRegex, $this->topic, $out);
+        return ($out[0] ?? null) === $this->topic;
+    }
+}
---
+++ tests/Recipient/TopicTest.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pk
+ * Date: 10.12.2017
+ * Time: 12:43
+ */
+
+namespace Jetifier\Tests\Recipient;
+
+use Jetifier\Exceptions\BadRecipientIdentifierException;
+use Jetifier\Recipient\Topic;
+use PHPUnit\Framework\TestCase;
+
+class TopicTest extends TestCase
+{
+    public function testGetIdentifier()
+    {
+        $identifier = 'ident';
+        $device = new Topic($identifier);
+        $this->assertEquals('/topics/' . $identifier, $device->getIdentifier());
+    }
+
+    public function testGetIdentifierEmptyException()
+    {
+        $device = new Topic('');
+        $this->expectException(BadRecipientIdentifierException::class);
+        $device->getIdentifier();
+    }
+
+    public function testGetIdentifierBadFormatException()
+    {
+        $device = new Topic('a b');
+        $this->expectException(BadRecipientIdentifierException::class);
+        $device->getIdentifier();
+    }
+
+
+    public function testGetIdentifierType()
+    {
+        $identifier = 1;
+        $device = new Topic($identifier);
+        $this->assertInternalType('string', $device->getIdentifier());
+    }
+}
