d997c99278c44e851a53a1b009fa490a27a23d15
PK <dev.franqy@gmail.com>
2017-12-10 11:25:18 +0100

    add constants

---
+++ src/Constants/Priority.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pk
+ * Date: 07.12.2017
+ * Time: 20:31
+ */
+
+namespace Jetifier\Constants;
+
+
+final class Priority
+{
+    public const NORMAL = 'NORMAL';
+    public const HIGH = 'HIGH';
+}
---
+++ src/Constants/SelfConfig.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pk
+ * Date: 09.12.2017
+ * Time: 10:10
+ */
+
+namespace Jetifier\Constants;
+
+
+final class Config
+{
+    public const API_URL = 'https://fcm.googleapis.com/fcm/send';
+}
